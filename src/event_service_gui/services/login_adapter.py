1d3ac2a6494830283857a1f9d6f13bd95726443d
heming-langrenn <info.renn.langrenn.kjelsaas@gmail.com>
2021-07-05 00:23:12 +0200

    Create new user #1

--- src/event_service_gui/services/login_adapter.py
+++ src/event_service_gui/services/login_adapter.py
@@ -2,7 +2,7 @@
 import logging
 import os
 
-from aiohttp import ClientSession, hdrs
+from aiohttp import ClientSession, hdrs, web
 from aiohttp_session import Session
 from multidict import MultiDict
 
@@ -13,6 +13,41 @@ USER_SERVICE_URL = f"http://{USER_SERVICE_HOST}:{USER_SERVICE_PORT}"
 
 class LoginAdapter:
     """Class representing login."""
+
+    async def create_user(
+        self,
+        token: str,
+        username: str,
+        password: str,
+        role: str,
+        cookiestorage: Session,
+    ) -> int:
+        """Create user function."""
+        id = ""
+        request_body = {
+            "username": username,
+            "password": password,
+            "role": role,
+        }
+        headers = MultiDict(
+            {
+                hdrs.CONTENT_TYPE: "application/json",
+                hdrs.AUTHORIZATION: f"Bearer {token}",
+            },
+        )
+        async with ClientSession() as session:
+            async with session.post(
+                f"{USER_SERVICE_URL}/users", headers=headers, json=request_body
+            ) as resp:
+                if resp.status == 201:
+                    logging.debug(f"create user - got response {resp}")
+                    location = resp.headers[hdrs.LOCATION]
+                    id = location.split(os.path.sep)[-1]
+                else:
+                    logging.error(f"create_event failed - {resp.status}")
+                    raise web.HTTPBadRequest(reason="Create event failed.")
+
+        return id
 
     async def login(self, username: str, password: str, cookiestorage: Session) -> int:
         """Perform login function."""
--- src/event_service_gui/views/login.py
+++ src/event_service_gui/views/login.py
@@ -3,6 +3,7 @@ import logging
 
 from aiohttp import web
 import aiohttp_jinja2
+from aiohttp_session import get_session
 from aiohttp_session import new_session
 
 from event_service_gui.services import LoginAdapter
@@ -13,17 +14,39 @@ class Login(web.View):
 
     async def get(self) -> web.Response:
         """Get route function that return the index page."""
+        username = ""
         try:
-            event = self.request.rel_url.query["event"]
+            informasjon = self.request.rel_url.query["informasjon"]
         except Exception:
-            event = ""
+            informasjon = ""
+        try:
+            eventid = self.request.rel_url.query["eventid"]
+        except Exception:
+            eventid = ""
+
+        try:
+            create_new = False
+            new = self.request.rel_url.query["new"]
+            if new != "":
+                session = await get_session(self.request)
+                loggedin = LoginAdapter().isloggedin(session)
+                if loggedin:
+                    create_new = True
+                    username = session["username"]
+
+        except Exception:
+            create_new = False
 
         return await aiohttp_jinja2.render_template_async(
             "login.html",
             self.request,
             {
                 "lopsinfo": "Login",
-                "event": event,
+                "event": [],
+                "eventid": eventid,
+                "informasjon": informasjon,
+                "username": username,
+                "create_new": create_new,
             },
         )
 
@@ -41,16 +64,34 @@ class Login(web.View):
             except Exception:
                 eventid = ""
 
-            # Perform login
-            session = await new_session(self.request)
-            result = await LoginAdapter().login(
-                form["username"], form["password"], session
-            )
-            if result != 200:
-                informasjon = "Innlogging feilet"
+            # Create new event
+            if "create" in form.keys():
+                session = await get_session(self.request)
+                token = session["token"]
+                result = await LoginAdapter().create_user(
+                    token,
+                    form["newrole"],
+                    form["newusername"],
+                    form["newpassword"],
+                    session,
+                )
+                informasjon = f"Ny bruker opprettet med id {id}"
+                return web.HTTPSeeOther(
+                    location=f"/login?new=True&informasjon={informasjon}"
+                )
+
+            else:
+                # Perform login
+                session = await new_session(self.request)
+                result = await LoginAdapter().login(
+                    form["username"], form["password"], session
+                )
+                if result != 200:
+                    informasjon = "Innlogging feilet"
 
         except Exception:
             logging.error("Error handling post - login")
+            result = 400
 
         if result != 200:
             return await aiohttp_jinja2.render_template_async(
