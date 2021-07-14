1193f89865a6278a8ce7f09c0d74bedbc8e09904
heming-langrenn <info.renn.langrenn.kjelsaas@gmail.com>
2021-07-14 09:20:21 +0200

    Logout function ¤3

--- src/event_service_gui/views/__init__.py
+++ src/event_service_gui/views/__init__.py
@@ -3,6 +3,7 @@ from .contestants import Contestants
 from .events import Events
 from .liveness import Ping, Ready
 from .login import Login
+from .logout import Logout
 from .main import Main
 from .raceclasses import Raceclasses
 from .schedules import Schedules
