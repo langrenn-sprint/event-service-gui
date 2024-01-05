f10cc1449100d7962791ea9ab4c8f39ba66ade4f
heming-langrenn <heming_harnes@hotmail.com>
2024-01-05 10:09:52 +0100

    Ved sletting av valge deltakere, blir kun 1 slettet. #143

--- event_service_gui/views/contestants.py
+++ event_service_gui/views/contestants.py
@@ -167,14 +167,19 @@ class Contestants(web.View):
                 informasjon = "Sletting utført: "
                 for key in form.keys():
                     if key.startswith("slett_"):
-                        contestant_id = str(form[key])
-                        contestant = await ContestantsAdapter().get_contestant(
-                            user["token"], event_id, contestant_id
-                        )
-                        result = await ContestantsAdapter().delete_contestant(
-                            user["token"], event_id, contestant
-                        )
-                        informasjon += f"{key} "
+                        try:
+                            contestant_id = str(form[key])
+                            contestant = await ContestantsAdapter().get_contestant(
+                                user["token"], event_id, contestant_id
+                            )
+                            result = await ContestantsAdapter().delete_contestant(
+                                user["token"], event_id, contestant
+                            )
+                            informasjon += f"OK:{key} "
+                        except Exception as e:
+                            informasjon += f"Feil:{key}({e}) "
+
+            
             elif "delete_all" in form.keys():
                 result = await ContestantsAdapter().delete_all_contestants(
                     user["token"], event_id
